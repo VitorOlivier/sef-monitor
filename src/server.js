9a73f930b312322e09a919a2ca10d3f9a93223fd
Andrey Luiz <andreyluiz.developer@gmail.com>
2019-10-28 11:06:50 +0000

    Apply eslint and prettier

--- src/server.js
+++ src/server.js
@@ -1,59 +1,52 @@
-const config         = require('./cfg/config.json');
-var   agendamentos   = new Object();
+const express = require('express');
+const bodyParser = require('body-parser');
+const schedule = require('node-schedule');
+const config = require('./cfg/config.json');
+const scraper = require('./scraper');
+
+var agendamentos = new Object();
 
 //Logger = gerenciador de log para armazenar em arquivo log em disco
 const winston = require('winston');
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      winston.format.json(),
-    ),
-    transports: [
-      new winston.transports.Console({
-        colorize: true
-      }),
-      new winston.transports.File({ filename: __dirname+'/cfg/log.json' })
-    ]
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console({
+      colorize: true,
+    }),
+    new winston.transports.File({ filename: __dirname + '/cfg/log.json' }),
+  ],
 });
 
-//Instancia do Servidor Web
-const express        = require('express');
-const bodyParser     = require('body-parser');
 var server = express();
-//Adiciona apis ao Servidor
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));  
 
-//Define Rota getAgendamento
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
 server.get(config.urlGetDatasAgendamento, async (req, res) => {
-    try{
-        return res.status(200).json(agendamentos);    
-    }catch(err){
-        logger.error({erro : err.message});
-        return res.status(400).send({erro : err.message});  
-    };
+  try {
+    return res.status(200).json(agendamentos);
+  } catch (e) {
+    logger.error(e);
+    return res.status(400).send({ error: e.message });
+  }
 });
 
-//Inicia Servidor
 server.listen(config.port, () => {
-    logger.info('SEF-Monitor ouvindo a porta '+config.port);
-    });
+  logger.info('SEF-Monitor ouvindo a porta ' + config.port);
+});
 
-//Inicia scheduler que mantem a raspagem
-const schedule       = require('node-schedule');
-const scraper        = require('./scraper');
-let j = schedule.scheduleJob('*/10 * * * *', async () => {
-    agendamentos = await scraper.scraping();
+schedule.scheduleJob('*/10 * * * *', async () => {
+  agendamentos = await scraper.scraping();
 });
 
 //Teste - Inicia raspagem sem scheduler
-(async () => {
-    agendamentos = await scraper.scraping()
-})()
-
-
-
-module.exports.logger = logger;   
+// (async () => {
+//   agendamentos = await scraper.scraping();
+// })();
