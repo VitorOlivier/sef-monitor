cf072a700bb36b441f0f5d062cd5b6fe33af21ac
Vitor Olivier <vitorollivier@gmail.com>
2019-12-08 11:24:53 +0000

    fix mongodb file setup path

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const mongoose = require('mongoose');
 
-const serviceAccount = require(path.resolve(__dirname, './cfg/sef-monitor-mongoose.json'));
+const serviceAccount = require(path.resolve(__dirname, '../cfg/sef-monitor-mongoose.json'));
 
 mongoose.connect(serviceAccount.uris, serviceAccount.options);
 
