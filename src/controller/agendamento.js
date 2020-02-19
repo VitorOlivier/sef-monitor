27769a13d73b74ea9286dc37ea40afff661a8026
Vitor Olivier <vitorollivier@gmail.com>
2020-02-19 11:44:39 +0000

    new sources

--- src/controller/agendamento.js
+++ src/controller/agendamento.js
@@ -8,17 +8,35 @@ router.post('/', async (req, res) => {
     const agendamento = await Agendamento.create(req.body);
     return res.send({ agendamento });
   } catch (err) {
-    return res.status(400).send({ err });
+    return res.status(500).send({ err });
   }
 });
 
 router.get('/', async (req, res) => {
-  try {
-    const agendamentos = await Agendamento.find(req.body.filter).sort(req.body.sort);
-    return res.send({ agendamentos });
-  } catch (err) {
-    return res.status(400).send({ err });
-  }
+  const pageSize = parseInt(req.query.pageSize) || 20;
+  const currentPage = parseInt(req.query.page) > 0 ? parseInt(req.query.page) - 1 : 0;
+  const sortBy = req.query.sortBy || 'hora';
+  const orderBy = req.query.orderBy || 'asc';
+  const sortQuery = { [sortBy]: orderBy };
+  const data = req.query.data;
+  const hora = req.query.hora;
+  const local = req.query.local;
+  const servico = req.query.servico;
+  let filterQuery = {};
+  filterQuery = data ? { ...filterQuery, data } : filterQuery;
+  filterQuery = hora ? { ...filterQuery, hora } : filterQuery;
+  filterQuery = local ? { ...filterQuery, local } : filterQuery;
+  filterQuery = servico ? { ...filterQuery, servico } : filterQuery;
+  Agendamento.find(filterQuery)
+    //.limit(pageSize)
+    .skip(currentPage * pageSize)
+    .sort(sortQuery)
+    .then(agendamentos => {
+      return res.status(200).json({ agendamentos });
+    })
+    .catch(err => {
+      return res.status(500).json({ err });
+    });
 });
 
 module.exports.routers = app => app.use('/agendamento', router);
