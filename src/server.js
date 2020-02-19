50557f00456cd5ef3acbfb9d9f76b8a335b981cd
Vitor Olivier <vitorollivier@gmail.com>
2020-02-19 23:11:59 +0000

    fix app_url variable

--- src/server.js
+++ src/server.js
@@ -5,10 +5,15 @@ const schedule = require('node-schedule');
 const scraper = require('./scraper');
 const logger = require('./logger');
 const config = require('./cfg/config.json');
+const appURL = (process.env.APP_URL, 'https://sef-monitor-web.herokuapp.com/');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.redirect(appURL);
+});
 
 require('./controller/agendamento').routers(app);
 
